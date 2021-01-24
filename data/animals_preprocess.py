ab20739e0cb55859327433bcc2f904a59b000edf
Jan Ruettinger <service+github@janruettinger.com>
2021-01-24 23:31:31 +0000

    fix small bugs after trial and error

--- data/animals_preprocess.py
+++ data/animals_preprocess.py
@@ -1,14 +1,14 @@
 from PIL import Image
 from pathlib import Path
-import shutil
-import split_folders
+import os
+import splitfolders
 
 
 IMG_DIM = 64
-INPUT_PATH = "/scratch/local/ssd/janhr/data/animals_original" 
-OUTPUT_PATH = "/scratch/local/ssd/janhr/data/animals" 
+INPUT_PATH = "/scratch/local/ssd/janhr/data/animals_original/" 
+OUTPUT_PATH = "/scratch/local/ssd/janhr/data/animals/" 
 
-# Helper fucntion
+#### Helper fucntion ####
 def crop_center(pil_img, crop_width, crop_height):
     img_width, img_height = pil_img.size
     return pil_img.crop(((img_width - crop_width) // 2,
@@ -16,45 +16,54 @@ def crop_center(pil_img, crop_width, crop_height):
                          (img_width + crop_width) // 2,
                          (img_height + crop_height) // 2))
 
-# Helper function
 def crop_max_square(pil_img):
     return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
 
+def drop_empty_folders(directory):
+    """Verify that every empty folder removed in local storage."""
 
-# 1. Split dataset into train/val/test
-split_folders.ratio(input=INPUT_PATH, output=OUTPUT_PATH, seed=1337, ratio=(.8, 0.1,0.1))
+    for dirpath, dirnames, filenames in os.walk(directory, topdown=False):
+        if not dirnames and not filenames:
+            os.rmdir(dirpath)
+
+def preprocess_and_move_images(data_path):
+    for each_file in data_path.glob('**/*.jpg'): # grabs all files
+        trg_path = each_file.parent.parent # gets the parent of the folder 
+
+        img = Image.open(str(each_file))
+        # Crop max square from image
+        img_cropped = crop_max_square(img)
+        # Resize image to 64x64
+        img_resized = img_cropped.resize((IMG_DIM,IMG_DIM))
+
+        img_resized.save(str(each_file), quality=95)
+        each_file.rename(data_path.joinpath(each_file.name)) # moves to parent folder
+
+# # 1. Split dataset into train/val/test
+splitfolders.ratio(INPUT_PATH, output=OUTPUT_PATH, seed=1337, ratio=(.8, 0.1,0.1))
 
 print("Images were split into train, val and test.")
 
-# 2. Move and resize images
-    # 1. Resize and crop images
-    # 2. Move images from train/class_name/img.jpg to train/img.jpg
-train_path = OUTPUT_PATH+"/train"
-val_path = OUTPUT_PATH+"/val"
-test_path = OUTPUT_PATH+"/test"
+# # 2. Move and resize images
+#     # 1. Resize and crop images
+#     # 2. Move images from train/class_name/img.jpg to train/img.jpg
+train_path = Path(OUTPUT_PATH+"/train")
+val_path = Path(OUTPUT_PATH+"/val")
+test_path = Path(OUTPUT_PATH+"/test")
 
-
-for each_file in Path(train_path).glob('**/*.jpg') # grabs all files
-    trg_path = each_file.parent.parent # gets the parent of the folder 
-
-    # Crop max square from image
-    im_cropped = crop_max_square(im)
-
-    # Resize image to 64x64
-    im_resized = img_cropped.resize((IMG_DIM,IMG_DIM))
-
-    im_resized.save(str(trg_path), quality=95)
-    each_file.rename(trg_path.joinpath(each_file.name)) # moves to parent folder.
+preprocess_and_move_images(train_path)
+preprocess_and_move_images(val_path)
+preprocess_and_move_images(test_path)
 
 print("Images are now in the correct shape and size as well as in the correct folder structure.")
 
-for folder in Path(train_path).glob("**")
-    Path.rmdir(folder)
+# sanity check
+num_files = len(list(Path(OUTPUT_PATH).glob('**/*.jpg')))
+assert num_files == 117484, "Not all images were found" 
 
-for folder in Path(val_path).glob("**")
-    Path.rmdir(folder)
 
-for folder in Path(test_path).glob("**")
-    Path.rmdir(folder)
+drop_empty_folders(str(train_path))
+drop_empty_folders(str(val_path))
+drop_empty_folders(str(test_path))
 
 print("Unnecessary folders were deleted. Everything complete.")
