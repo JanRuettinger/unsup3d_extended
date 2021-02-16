1f49c108733e5c82922eb3cb63a23274c27f211b
Jan Ruettinger <service+github@janruettinger.com>
2021-02-16 10:18:13 +0000

    add first version of new renderer class that runs but results still wrong

--- unsup3d/renderer/renderer.py
+++ unsup3d/renderer/renderer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import torch
 import torch.nn as nn
 import pytorch3d
@@ -7,82 +8,107 @@ import pytorch3d.renderer
 import pytorch3d.structures
 import pytorch3d.io
 import pytorch3d.transforms
-​
-​
+from pytorch3d.renderer import (
+    RasterizationSettings,
+    MeshRenderer,
+    SoftPhongShader,
+    MeshRasterizer,
+    TexturesUV,
+    DirectionalLights
+)
+from . import utils
+
 class Renderer(nn.Module):
     def __init__(self, cfgs):
         super().__init__()
         self.cfgs = cfgs
         self.device = cfgs.get('device', 'cpu')
-        self.render_resolution = cfgs.get('render_resolution', 64)
+        self.image_size = cfgs.get('image_size', 64)
         self.fov = cfgs.get('fov', 10)
-        self.blur_raduis = cfgs.get('blend_radius', np.log(1. / 1e-4 - 1.)*1e-4)
+        self.blur_radius = cfgs.get('blur_radius', np.log(1. / 1e-4 - 1.)*1e-4)
         self.cameras = pytorch3d.renderer.FoVPerspectiveCameras(fov=self.fov, device=self.device)
         self.image_renderer = self._create_image_renderer()
-        init_verts, init_faces, init_aux = pytorch3d.io.load_obj(cfgs['init_shape.obj'], device=self.device)
+        init_verts, init_faces, init_aux = pytorch3d.io.load_obj(cfgs['init_shape_obj_path'], device=self.device)
         self.tex_faces_uv = init_faces.textures_idx.unsqueeze(0)
         self.tex_verts_uv = init_aux.verts_uvs.unsqueeze(0)
-​
+        fx = (self.image_size-1)/2/(math.tan(self.fov/2 *math.pi/180))
+        fy = (self.image_size-1)/2/(math.tan(self.fov/2 *math.pi/180))
+        cx = (self.image_size-1)/2
+        cy = (self.image_size-1)/2
+        K = [[fx, 0., cx],
+             [0., fy, cy],
+             [0., 0., 1.]]
+        K = torch.FloatTensor(K).to(self.device)
+        self.inv_K = torch.inverse(K).unsqueeze(0)
 
     def _create_image_renderer(self):
-        raster_settings = _get_rasterization_settings()
+        raster_settings = self._get_rasterization_settings()
         renderer = MeshRenderer(rasterizer=MeshRasterizer(cameras=self.cameras, raster_settings=raster_settings),
                                 shader=SoftPhongShader(device=self.device, cameras=self.cameras))
         return renderer
 
-
     def _get_rasterization_settings(self):
-        raster_settings = RasterizationSettings(image_size=self.render_resolution, blur_radius=self.blur_radius, faces_per_pixel=1)
+        raster_settings = RasterizationSettings(image_size=self.image_size, blur_radius=self.blur_radius, faces_per_pixel=1)
         return raster_settings
 
-
-    def _get_textures(self, tex_im)
+    def _get_textures(self, tex_im):
         # tex_im is a tensor that contains the non-flipped and flipped albedo_map
+        tex_im = tex_im.permute(0,2,3,1)
         b, h, w, c = tex_im.shape
-        assert w == self.render_resolution and h == self.render_resolution, "Texture image has the wrong resolution."
+        assert w == self.image_size and h == self.image_size, "Texture image has the wrong resolution."
         # tex_maps = tex_im.permute(0,2,3,1)
-        textures = pytorch3d.renderer.TexturesUV(maps=tex_im,  # texture maps are BxHxWx3
+        textures = TexturesUV(maps=tex_im,  # texture maps are BxHxWx3
                                                  faces_uvs=self.tex_faces_uv.repeat(b, 1, 1),
                                                  verts_uvs=self.tex_verts_uv.repeat(b, 1, 1))
         return textures
 
     
-    def _get_lights(self, light_direction, ambient_color, diffuse_color, specular_color=[0,0,0]):
-        ambient_color = torch.cat(3*[canon_light_a]).view(1,3)
-        diffuse_color = torch.cat(3*[canon_light_b]).view(1,3)
-        specular_color=torch.zeros((1,3))
+    def _get_lights(self, lightning):
+        ambient = lightning["ambient"]
+        diffuse = lightning["diffuse"]
+        direction = lightning["direction"]
+        ambient_color = ambient.repeat(1,3)
+        diffuse_color = diffuse.repeat(1,3)
+        b, _  = ambient.shape
+        specular_color=torch.zeros((b,3))
         lights = DirectionalLights(ambient_color=ambient_color, diffuse_color=diffuse_color, specular_color=specular_color, direction=direction)
-        return lights
+        return lights.to(self.device)
     
 
-    def _get_transformed_mesh(self, meshes, view):
+    def _get_transformed_meshes(self, meshes, view):
         # rotate mesh with pytorch functions
         # rotate non flipped mesh in one direction and flipped version in other direction
 
-        R = axis_angle_to_matrix(view[:,:3])
-        T = view[:,3:
+        R = pytorch3d.transforms.euler_angles_to_matrix(view[:,:3], convention="XYZ")
+        T = view[:,3:]
 
         object_center = torch.tensor([[0.0,0.0,1.0]])
-        shift_center = pytorch3d.transforms.Translate(-object_center, device=device)
-        rotate = pytorch3d.transforms.Rotate(R, device=device)
-        shift_back = pytorch3d.transforms.Translate(object_center, device=device)
-        translate = pytorch3d.transforms.Translate(T, device=device)
+        shift_center = pytorch3d.transforms.Translate(-object_center, device=self.device)
+        rotate = pytorch3d.transforms.Rotate(R, device=self.device)
+        shift_back = pytorch3d.transforms.Translate(object_center, device=self.device)
+        translate = pytorch3d.transforms.Translate(T, device=self.device)
         tsf = shift_center.compose(rotate, shift_back, translate)
 
         meshes_verts = meshes.verts_padded()
         new_mesh_verts = tsf.transform_points(meshes_verts)
         transformed_meshes = meshes.update_padded(new_mesh_verts) 
+        return meshes.to(self.device)
+
+    def create_meshes_from_depth_map(self,depth_map):
+
+        grid_3d = utils.depth_to_3d_grid(depth_map, self.inv_K)
+        meshes = utils.create_meshes_from_grid_3d(grid_3d, self.device)
         return meshes
+
 
     def forward(self, meshes, albedo_maps, view, lightning):
         # Can both images (flipped and not flipped be rendered in one go?)
-
-        textures = self.get_textures(albedo_maps)
-        lights = self.get_lights(lightning)
-        transformed_mesh = self.get_transformed_meshes(meshes, view)
+        textures = self._get_textures(albedo_maps)
+        lights = self._get_lights(lightning)
+        transformed_meshes = self._get_transformed_meshes(meshes, view)
 
         # replace texture at mesh
-        self.transformed_mesh.textures = textures
+        transformed_mesh.textures = textures
 
-        images = self.image_renderer(meshes_world=transformed_mesh,lights=lights)
+        images = self.image_renderer(meshes_world=transformed_meshes,lights=lights)
         return images
--- unsup3d/renderer/utils.py
+++ unsup3d/renderer/utils.py
@@ -7,9 +7,40 @@ import pytorch3d.renderer
 import pytorch3d.structures
 import pytorch3d.io
 import pytorch3d.transforms
-​
 
+def create_meshes_from_grid_3d(grid_3d, device):
+    ## Vertices
+    vertices = grid_3d 
+    b, h, w, _ = vertices.shape
+    vertices_center = torch.nn.functional.avg_pool2d(vertices.permute(0,3,1,2), 2, stride=1).permute(0,2,3,1)
+    vertices = torch.cat([vertices.view(b,h*w,3), vertices_center.view(b,(h-1)*(w-1),3)], 1)
 
-def create_meshes_from_vertices_and_faces(vertices, faces):
-    meshes = pytorch3d.structures.Meshes(verts=vertices.to(self.device), faces=faces.to(self.device), textures=textures.to(self.device))
+    ## Faces
+    idx_map = torch.arange(h*w).reshape(h,w)
+    idx_map_center = torch.arange((h-1)*(w-1)).reshape(h-1,w-1)
+    faces1 = torch.stack([idx_map[:h-1,:w-1], idx_map[1:,:w-1], idx_map_center+h*w], -1).reshape(-1,3).repeat(b,1,1).int()  # Bx((H-1)*(W-1))x4
+    faces2 = torch.stack([idx_map[1:,:w-1], idx_map[1:,1:], idx_map_center+h*w], -1).reshape(-1,3).repeat(b,1,1).int()  # Bx((H-1)*(W-1))x4
+    faces3 = torch.stack([idx_map[1:,1:], idx_map[:h-1,1:], idx_map_center+h*w], -1).reshape(-1,3).repeat(b,1,1).int()  # Bx((H-1)*(W-1))x4
+    faces4 = torch.stack([idx_map[:h-1,1:], idx_map[:h-1,:w-1], idx_map_center+h*w], -1).reshape(-1,3).repeat(b,1,1).int()  # Bx((H-1)*(W-1))x4
+    faces = torch.cat([faces1, faces2, faces3, faces4], 1)
+
+    meshes = pytorch3d.structures.Meshes(verts=vertices.to(device), faces=faces.to(device))
     return meshes
+
+def get_grid(b, H, W, normalize=True):
+    if normalize:
+        h_range = torch.linspace(-1,1,H)
+        w_range = torch.linspace(-1,1,W)
+    else:
+        h_range = torch.arange(0,H)
+        w_range = torch.arange(0,W)
+    grid = torch.stack(torch.meshgrid([h_range, w_range]), -1).repeat(b,1,1,1).flip(3).float() # flip h,w to x,y
+    return grid 
+
+def depth_to_3d_grid(depth, inv_K):
+    b, h, w = depth.shape
+    grid_2d = get_grid(b, h, w, normalize=False).to(depth.device)  # Nxhxwx2
+    depth = depth.unsqueeze(-1)
+    grid_3d = torch.cat((grid_2d, torch.ones_like(depth)), dim=3)
+    grid_3d = grid_3d.matmul(inv_K.transpose(2,1)) * depth
+    return grid_3d
