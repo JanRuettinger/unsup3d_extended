27664412a63320fc3f62c820e49a173c0fd6b81e
Jan Ruettinger <service+github@janruettinger.com>
2021-02-18 09:03:35 +0000

    updated debug code

--- unsup3d/renderer/renderer.py
+++ unsup3d/renderer/renderer.py
@@ -69,7 +69,9 @@ class Renderer(nn.Module):
     def _get_lights(self, lightning):
         ambient = lightning["ambient"]/2.+0.5
         diffuse = lightning["diffuse"]/2.+0.5
-        direction = lightning["direction"]
+        direction = -lightning["direction"]
+        # TODO: DEBUG
+        direction = torch.tensor([[ 0.2878, -0.1185, -0.9503]]*64)
         ambient_color = ambient.repeat(1,3)
         diffuse_color = diffuse.repeat(1,3)
         b, _  = ambient.shape
